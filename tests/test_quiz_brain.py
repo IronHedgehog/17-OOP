03804993e7856b8d1537d688a04a422837da351a
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2025-12-26 18:30:28 +0200

    remove comments

--- tests/test_quiz_brain.py
+++ tests/test_quiz_brain.py
@@ -5,7 +5,6 @@ def test_check_answer_correct():
     q = Question("Test question?", "True")
     quiz = QuizBrain([q])
 
-    # перевіряємо правильну відповідь
     result = quiz.check_answer("true", "True")
     assert result is True
     assert quiz.score == 1
@@ -14,7 +13,6 @@ def test_check_answer_incorrect():
     q = Question("Test question?", "True")
     quiz = QuizBrain([q])
 
-    # перевіряємо неправильну відповідь
     result = quiz.check_answer("false", "True")
     assert result is False
     assert quiz.score == 0
